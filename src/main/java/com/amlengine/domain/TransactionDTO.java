196b5a5c06a47bda7cbea910add94b2d218560b8
ggeuri <geurisbebe@gmail.com>
2025-11-18 23:14:36 +0900

    feat: Create TransactionDTO and domain enum structure

---
+++ src/main/java/com/amlengine/domain/TransactionDTO.java
@@ -0,0 +1,30 @@
+package main.java.com.amlengine.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class TransactionDTO {
+    private long uid;
+    private TransactionType type;
+    private long amountKrw;
+    private LocalDateTime transactedAt;
+    
+    // 코인/매매 전용(assetQuantity*quotePriceKrw = amountKrw)
+    private String assetSymbol;
+    private BigDecimal assetQuantity;
+    private BigDecimal quotePriceKrw;  // 코인 1단위 기준 KRW 시세 
+    
+    
+    // Coin 입출고 전용
+    private String fromAddress;
+    private String toAddress;
+    
+    //긴건 뒤로 빼기 
+    private String countryCode; //거래 필수조건 아님 뒤로 
+    private String txId;
+    private String ipAddress;
+
+
+}
+
+
---
+++ src/main/java/com/amlengine/domain/TransactionType.java
@@ -0,0 +1,10 @@
+package main.java.com.amlengine.domain;
+
+public enum TransactionType {
+    KRW_DEPOSIT,
+    KRW_WITHDRAW,
+    COIN_DEPOSIT,
+    COIN_WITHDRAW,
+    TRADE_BUY,
+    TRADE_SELL
+}
